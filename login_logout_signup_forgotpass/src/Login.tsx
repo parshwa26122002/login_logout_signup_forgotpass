79282f12d4f7880b38db4b137851c23f38e64aa5
Parshwa Shah <parshwa26122002@gmail.com>
2023-07-02 04:54:11 +0100

    Login cleaned

--- login_logout_signup_forgotpass/src/Login.tsx
+++ login_logout_signup_forgotpass/src/Login.tsx
@@ -6,9 +6,10 @@ function Login() {
     const [ showTooltip, setShowTooltip ] = useState(false);
     const target = useRef(null);
     const emailtarget = useRef(null);
+    const regex = /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/;
 
     function passwordValidator(event:any) {
-        if(event.target.value.match(/(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/)) {
+        if(event.target.value.match(regex)) {
             setShowTooltip(false);
             if((emailtarget.current as any).value != "") {
                 setIsDisabled(false);
@@ -26,27 +27,29 @@ function Login() {
 
     return (
       <>
-        <Form onSubmit={(event) => submitHandler(event)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control ref={emailtarget} type="email" placeholder="Enter email" onChange={(event) => {event.target.value != "" && (target.current as any).value.match(/(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/) ? setIsDisabled(false) : setIsDisabled(true)}} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control ref={target} type="password" placeholder="Password" onChange={(event) => passwordValidator(event)}/>
-                <Overlay target={target.current} show={showTooltip} placement="bottom">
-                    {(props) => (
-                        <Tooltip id="overlay-example" {...props}>
-                            At least 1 uppercase, 1 lowercase, 1 digit and 1 special character is required.{'\n'}
-                            Minimum 8 characters are required.
-                        </Tooltip>
-                    )}
-                </Overlay>
-            </Form.Group>
-            <Button variant="primary" type="submit" disabled={isDisabled}>
-                Login
-            </Button>
-        </Form>
+        <div className='d-flex flex-column align-items-center'>
+            <h1 className='fs-3 m-3'>Login</h1>
+            <Form className="w-25" onSubmit={(event) => submitHandler(event)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailtarget} type="email" placeholder="Enter email" onChange={(event) => {event.target.value != "" && (target.current as any).value.match(regex) ? setIsDisabled(false) : setIsDisabled(true)}} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={target} type="password" placeholder="Password" onChange={(event) => passwordValidator(event)}/>
+                    <Overlay target={target.current} show={showTooltip} placement="bottom">
+                        {(props) => (
+                            <Tooltip id="overlay-example" {...props}>
+                                At least 1 uppercase, 1 lowercase, 1 digit and 1 special character is required. Minimum 8 characters are required.
+                            </Tooltip>
+                        )}
+                    </Overlay>
+                </Form.Group>
+                <Button className="w-100" variant="primary" type="submit" disabled={isDisabled}>
+                    Login
+                </Button>
+            </Form>
+        </div>
       </>
     )
 }
